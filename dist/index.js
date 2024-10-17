6db913fbb0bd11bede019ce965c2f0aaf8aef355
Jupower38300 <julien.legrand@my-digital-school.org>
2024-10-17 13:29:16 +0200

    Changements sur le déploiement

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
+import _client from "react-dom/client";
+import _reactRouterDom from "react-dom/client";
 
-var _client = require("react-dom/client");
-var _reactRouterDom = require("react-router-dom");
+
 var _App = _interopRequireDefault(require("./App"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 const rootElement = document.getElementById("root");
