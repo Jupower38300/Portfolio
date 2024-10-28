e650bd1d053a701315d786e2de447fa23945d86a
Jupower38300 <julien.legrand@my-digital-school.org>
2024-10-28 10:44:46 +0100

    Mise en place du dossier public

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: './', // Définit la racine du projet
+  publicDir: 'public', // Indique à Vite où se trouve le dossier public
+  plugins: [react()],
+});
   
